d5b57b7c1939219dd5a88527fe7ec5ba71942bee
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-11-30 14:55:45 -0500

    Added publisher.hpp, modified publisher.cpp
    1. Created include/beginner_tutorials
    2. Added publisher.hpp header for talker node
    3. Modified publisher.cpp to utilize header file

--- beginner_tutorials/src/publisher.cpp
+++ beginner_tutorials/src/publisher.cpp
@@ -25,132 +25,106 @@ SOFTWARE.
 /**
  * @copyright Copyright (c) 2022
  * @file publisher.cpp
- * @author Tanuj Thakkar (tanuj@phantomauto.com)
+ * @author Tanuj Thakkar (tanuj@umd.edu)
  * @version 0.1
- * @date 2022-11-06
  *
- * @brief ROS2 Publisher
+ * @brief ROS2 Publisher source file
  *
  */
 
-#include <chrono>
-#include <functional>
-#include <memory>
-#include <rcl_interfaces/msg/detail/parameter_descriptor__struct.hpp>
-#include <string>
-
-#include "beginner_tutorials/srv/count.hpp"
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-
-using namespace std::chrono_literals;
-using namespace std::placeholders;
-using Count = beginner_tutorials::srv::Count;
+#include <beginner_tutorials/publisher.hpp>
 
 /**
- * @Brief Class to represent publisher and service
- */
-class MinimalPublisher : public rclcpp::Node {
- public:
-  /**
-   * @brief Construct a new Minimal Publisher object
-   *
-   */
-  MinimalPublisher() : Node("minimal_publisher"), count_(0) {
-    if (rcutils_logging_set_logger_level(
-            this->get_logger().get_name(),
-            RCUTILS_LOG_SEVERITY::RCUTILS_LOG_SEVERITY_DEBUG) ==
-        RCUTILS_RET_OK) {
-      RCLCPP_INFO_STREAM(this->get_logger(), "Set logger level DEBUG success.");
-    } else {
-      RCLCPP_ERROR_STREAM(this->get_logger(), "Set logger level DEBUG fails.");
-    }
-
-    auto param_desc = rcl_interfaces::msg::ParameterDescriptor{};
-    param_desc.description =
-        "\nThis parameter is the count value passed between "
-        "the minimal publisher and the minimal subscriber."
-        "\n\n It also determines the logger level of both nodes.";
-
-    this->declare_parameter("count", count_, param_desc);
-
-    count_ = this->get_parameter("count").get_parameter_value().get<int>();
-    RCLCPP_INFO_STREAM(this->get_logger(), "Starting counter from: " << count_);
-
-    publisher_ = this->create_publisher<std_msgs::msg::String>("talker", 10);
-    timer_ = this->create_wall_timer(
-        1s, std::bind(&MinimalPublisher::timer_callback, this));
-
-    // Create a service for modifying count
-    std::string get_count_service_name =
-        "/" + std::string(this->get_name()) + "/" + "Count";
-    get_count_service_ = this->create_service<Count>(
-        get_count_service_name,
-        std::bind(&MinimalPublisher::get_count_callback, this, _1, _2));
+  * @brief Construct a new Minimal Publisher object
+  *
+  */
+MinimalPublisher::MinimalPublisher() : Node("minimal_publisher"), count_(0) {
+  if (rcutils_logging_set_logger_level(
+          this->get_logger().get_name(),
+          RCUTILS_LOG_SEVERITY::RCUTILS_LOG_SEVERITY_DEBUG) ==
+      RCUTILS_RET_OK) {
+    RCLCPP_INFO_STREAM(this->get_logger(), "Set logger level DEBUG success.");
+  } else {
+    RCLCPP_ERROR_STREAM(this->get_logger(), "Set logger level DEBUG fails.");
   }
 
- private:
-  /**
-   * @brief Callback from timer
-   *
-   */
-  void timer_callback() {
-    auto message = std_msgs::msg::String();
-    count_ = this->get_parameter("count").get_parameter_value().get<int>();
-    message.data = std::to_string(count_);
-    this->logger(message);
-    publisher_->publish(message);
-    count_++;
-    this->set_parameter(rclcpp::Parameter("count", count_));
+  auto param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+  param_desc.description =
+      "\nThis parameter is the count value passed between "
+      "the minimal publisher and the minimal subscriber."
+      "\n\n It also determines the logger level of both nodes.";
+
+  this->declare_parameter("count", count_, param_desc);
+
+  count_ = this->get_parameter("count").get_parameter_value().get<int>();
+  RCLCPP_INFO_STREAM(this->get_logger(), "Starting counter from: " << count_);
+
+  publisher_ = this->create_publisher<std_msgs::msg::String>("talker", 10);
+  timer_ = this->create_wall_timer(
+      1s, std::bind(&MinimalPublisher::timer_callback, this));
+
+  // Create a service for modifying count
+  std::string get_count_service_name =
+      "/" + std::string(this->get_name()) + "/" + "Count";
+  get_count_service_ = this->create_service<Count>(
+      get_count_service_name,
+      std::bind(&MinimalPublisher::get_count_callback, this, _1, _2));
+}
+
+/**
+  * @brief Callback from timer
+  *
+  */
+void MinimalPublisher::timer_callback() {
+  auto message = std_msgs::msg::String();
+  count_ = this->get_parameter("count").get_parameter_value().get<int>();
+  message.data = std::to_string(count_);
+  this->logger(message);
+  publisher_->publish(message);
+  count_++;
+  this->set_parameter(rclcpp::Parameter("count", count_));
+}
+
+/**
+  * @brief Switch to reproduce logging levels
+  *
+  * @param msg
+  */
+void MinimalPublisher::logger(const std_msgs::msg::String &msg) {
+  int count = stoi(msg.data);
+  switch (count % 5) {
+  case 0:
+    RCLCPP_DEBUG_STREAM(this->get_logger(), "Count: " << msg.data);
+    break;
+  case 1:
+    RCLCPP_INFO_STREAM(this->get_logger(), "Count: " << msg.data);
+    break;
+  case 2:
+    RCLCPP_WARN_STREAM(this->get_logger(), "Count: " << msg.data);
+    break;
+  case 3:
+    RCLCPP_ERROR_STREAM(this->get_logger(), "Count: " << msg.data);
+    break;
+  case 4:
+    RCLCPP_FATAL_STREAM(this->get_logger(), "Count: " << msg.data);
+    break;
+  default:
+    break;
   }
 
-  /**
-   * @brief Switch to reproduce logging levels
-   *
-   * @param msg
-   */
-  void logger(const std_msgs::msg::String &msg) {
-    int count = stoi(msg.data);
-    switch (count % 5) {
-      case 0:
-        RCLCPP_DEBUG_STREAM(this->get_logger(), "Count: " << msg.data);
-        break;
-      case 1:
-        RCLCPP_INFO_STREAM(this->get_logger(), "Count: " << msg.data);
-        break;
-      case 2:
-        RCLCPP_WARN_STREAM(this->get_logger(), "Count: " << msg.data);
-        break;
-      case 3:
-        RCLCPP_ERROR_STREAM(this->get_logger(), "Count: " << msg.data);
-        break;
-      case 4:
-        RCLCPP_FATAL_STREAM(this->get_logger(), "Count: " << msg.data);
-        break;
-      default:
-        break;
-    }
+  return;
+}
 
-    return;
-  }
-
-  /**
-   * @brief Get the count callback object
-   *
-   * @param request
-   * @param response
-   */
-  void get_count_callback(const std::shared_ptr<Count::Request> request,
-                          std::shared_ptr<Count::Response> response) {
-    (void)request;
-    response->count = count_;
-  }
-
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  rclcpp::Service<Count>::SharedPtr get_count_service_;
-  int count_;
-};
+/**
+  * @brief Get the count callback object
+  *
+  * @param request
+  * @param response
+  */
+void MinimalPublisher::get_count_callback (const std::shared_ptr<Count::Request> request, std::shared_ptr<Count::Response> response) {
+  (void)request;
+  response->count = count_;
+}
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
